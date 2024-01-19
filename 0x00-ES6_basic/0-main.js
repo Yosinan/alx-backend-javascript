1bf6802fa18c224571a083aa478e1ce97c0ee1d4
Yosinan <ymymym4634@gmail.com>
2024-01-19 12:59:38 +0300

    ES6 Basics

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,7 +1,10 @@
 // import { taskFirst, taskNext } from './0-constants';
 // import taskBlock from './1-block-scoped.js'
 // import getNeighborhoodsList from './2-arrow.js';
-import getSumOfHoods from './3-default-parameter.js';
+// import getSumOfHoods from './3-default-parameter.js';
+import returnHowManyArguments from './4-rest-parameter.js';
+
+
 
 // console.log(`${taskFirst()}  ${taskNext()}`);
 
@@ -12,6 +15,9 @@ import getSumOfHoods from './3-default-parameter.js';
 // const res = neighborhoodsList.addNeighborhood('Noe Valley');
 // console.log(res);
 
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+// console.log(getSumOfHoods(34));
+// console.log(getSumOfHoods(34, 3));
+// console.log(getSumOfHoods(34, 3, 4));
+
+console.log(returnHowManyArguments("one"));
+console.log(returnHowManyArguments("one", "two", 3, "4th"));
